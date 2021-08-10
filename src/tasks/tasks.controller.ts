206af8a6f0ff2428b4ad21e6ac4ee8ff003c3d90
huongnt3 <huongnt3@vnext.com.vn>
2021-08-10 09:49:20 +0700

    Part 4 Getting and deleting a task by id

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { Task } from './tasks.model';
 import { TasksService } from './tasks.service';
@@ -12,10 +12,26 @@ export class TasksController {
     return this.tasksService.getAllTasks();
   }
 
+  @Get('/:id')
+  getTaskById(@Param('id') id: string): Task {
+    return this.tasksService.getTaskById(id);
+  }
+
   //Define parameter implicit
   @Post()
   createTask(@Body() createTaskDto: CreateTaskDto): Task {
     return this.tasksService.createTask(createTaskDto);
+  }
+
+  @Delete('/:id')
+  deleteTaskById(@Param('id') id: string): string {
+    const taskDeleted = this.tasksService.deleteTaskById(id);
+
+    if (!taskDeleted) {
+      return 'Somgthing went wrong when delete task!';
+    }
+
+    return `Task ${id} was deleted!`;
   }
 
   //Define parameter explicit
